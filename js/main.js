40cfe4190c8422487843f27aefe3caf33d5fbf5b
Мещеряков Роман <staremang@ya.ru>
2019-03-06 09:45:37 +0700

    1.1

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
-$(document).ready(function () {
-    // $('#fullpage').fullpage({
-    //     //options here
-    //     // autoScrolling: true,
-    //     // scrollHorizontally: true
-    // });
+function initCollapse() {
+    var showed = false;
+
+    $('.faq-collapse__title').on('click', function () {
+        toggleCollapse($(this).parents('.faq-collapse'));
+    });
+
+    function showCollapse($collapse) {
+        $collapse.find('.faq-collapse__container').outerHeight($collapse.find('.faq-collapse__body').outerHeight())
+        $collapse.addClass('active');
+
+        showed = true;
+    }
+    function hideCollapse($collapse) {
+
+        $collapse.find('.faq-collapse__container').outerHeight(0);
+        $collapse.removeClass('active');
+
+        showed = false;
+    }
+
+    function toggleCollapse($collapse) {
+        if ($collapse.hasClass('active')) {
+            hideCollapse($collapse);
+        } else {
+            hideCollapse($('.faq-collapse.active'));
+            showCollapse($collapse);
+        }
+    }
+}
+
+function initOnePage() {
+
     $('.section-1').addClass('viewed');
     // $('.header').addClass('animate');
 
@@ -30,22 +57,9 @@ $(document).ready(function () {
 
         $main.moveTo($(this).data('index'));
     })
-    // $(".main").onepage_scroll({
-    //     sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-    //     easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-    //                                      // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-    //     animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-    //     pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-    //     updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-    //     beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-    //     afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-    //     loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-    //     keyboard: true,                  // You can activate the keyboard controls
-    //     responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-    //     // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-    //     // the browser's width is less than 600, the fallback will kick in.
-    //     direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
-    // });
-    // methods
-    // $.fn.fullpage.setAllowScrolling(false);
+}
+
+$(document).ready(function () {
+    initOnePage();
+    initCollapse();
 });
